df206cdee96d204e6053e5e2aa1b469047f06313
Jose Chery <Jcareergoals@hotmail.com>
2016-08-04 13:48:27 -0400

    Delegated Ajax code to getData function that's evoked when the button is clicked

--- javascript.js
+++ javascript.js
@@ -1,9 +1,14 @@
 $(document).ready(function(){
-	$.get("https://api.github.com/users/jcareergoals", displayName);
 	function displayName(data){
 		console.log(data); 
 		for(i in data){
 			$('#content').append("<p><b>" + i + "</b>: " + data[i] + "</p>"); 
 		}
 	} 
+	function getData(){
+		$.get("https://api.github.com/users/jcareergoals", displayName);
+	}
+	$('#btn').click(function(){
+		getData(); 
+	}); 
 }); 		
