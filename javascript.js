f6c198b1512434eb7c596bfea8f9ccea618a81ef
Jose Chery <Jcareergoals@hotmail.com>
2016-08-04 16:59:31 -0400

    Implemented feature that allows users to search usernames against the api to recieve data

--- javascript.js
+++ javascript.js
@@ -1,14 +1,28 @@
 $(document).ready(function(){
+	// HANDLES RESPONSE
 	function displayName(data){
-		console.log(data); 
-		for(i in data){
-			$('#content').append("<p><b>" + i + "</b>: " + data[i] + "</p>"); 
+		if(typeof(data) === 'object'){
+			$('#content').html("");   // clears the #content element
+			for(i in data){
+				$('#content').append("<p><b>" + i + "</b>: " + data[i] + "</p>"); 
+			}
+		} else {
+			$('#content').html("<p>No data could be located for that username</p>"); 
 		}
 	} 
-	function getData(){
-		$.get("https://api.github.com/users/jcareergoals", displayName);
+	// GETS DATA FROM API 
+	function getData(data){
+		$.get("https://api.github.com/users/" + data, displayName);
+		$('#username').val(""); 
 	}
-	$('#btn').click(function(){
-		getData(); 
-	}); 
+	// FORM 
+	$('form').submit(function(){
+		var user = $('#username').val(); 
+		if(user.length > 1){ 
+			getData(user);	
+		} else {
+			$('#content').html("<p class='center'>Please enter a valid username (e.g. Jcareergoals )</p>");
+		}
+		return false; 	// prevents page from refreshing
+	});
 }); 		
